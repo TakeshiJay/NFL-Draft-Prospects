be10c7a03e3d5123356e266044fec869213d77f6
Jacob Sunia <70661851+TakeshiJay@users.noreply.github.com>
2021-08-18 13:13:44 -0700

    Created Prospects file

---
+++ Draft_22.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Prospect (
+    Player_Last_Name varchar(50),
+    Player_First_Name varchar(50),
+    Player_Position varchar(25),
+    Player_Height varchar(4),
+    Player_Weight varchar(3),
+    Player_University varchar(50)
+);
